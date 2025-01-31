79c6709f842e4c12810931481ab7f4ea825421e9
RameshNaik9 <lahorirameshnaik@gmail.com>
2025-02-01 01:09:34 +0530

    removed gauth login files

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import React from 'react';
 import RTSPSetup from './Pages/RTSPSetup';
 import VisitorVirtue from './Pages/VisitorVirtue';
 import LivePage from "./Pages/LivePage";
-import  LoginPage  from "./Pages/LoginPage"
 // import GraphPage from './Pages/GraphPage';
 
 const App = () => {
@@ -15,7 +14,6 @@ const App = () => {
    <Router>
     <Routes>
       <Route path="/" element={<LandingPage/>}/>
-      <Route path="/login" element={<LoginPage />} />  {/* Add Login Page Route */}
       <Route path="/RTSPSetup" element={<RTSPSetup/>}/>
       <Route path="/stream/:cameraId" element={<LivePage />} />
       <Route path="/VisitorVirtue" element={<VisitorVirtue/>}/>
