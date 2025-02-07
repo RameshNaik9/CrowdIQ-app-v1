6e5d8d85d1a0491037a315f3ac408a24f17de824
RameshNaik9 <lahorirameshnaik@gmail.com>
2025-02-08 03:41:54 +0530

    api health trigger cameras

--- routes/cameraRoutes.js
+++ routes/cameraRoutes.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const cameraController = require('../controllers/cameraController');
+const { performHealthCheck } = require('../services/healthCheckService');
+
 
 const router = express.Router();
+
+
+// ✅ Add API to trigger health check
+router.post('/trigger-health-check', async (req, res) => {
+    try {
+        await performHealthCheck();
+        res.status(200).json({ status: "Success", message: "Health check triggered." });
+    } catch (err) {
+        res.status(500).json({ status: "Fail", message: "Failed to perform health check." });
+    }
+});
 
 // ✅ Connect & Register a New Camera
 router.post('/connect', cameraController.connectCamera);
